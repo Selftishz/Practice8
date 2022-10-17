f8d2e5c201c368edb08a73044c399f2425e86f4c
Selftishz <art2sys048@gmail.com>
2022-10-17 18:41:11 +0300

    Commit

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,7 +1,27 @@
 package pack;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scan = new Scanner(System.in);
+    public static String Task1(int a, int b) {
+        if (a > b) {
+            if (a == b) {
+                return Integer.toString(a);
+            }
+            return a + " " + Task1(a - 1, b);
+        } else {
+            if (a == b) {
+                return Integer.toString(a);
+            }
+            return a + " " + Task1(a + 1, b);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("ЗАДАЧА 3\nВведите 2 числа: ");
+        int num1 = scan.nextInt(), num2 = scan.nextInt();
+        System.out.println(Task1(num1, num2));
+
     }
 }
